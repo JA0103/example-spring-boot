4e99cdc599e0419b5a1d512502946772da88c862
JA0103 <jinaa0103@gmail.com>
2026-04-06 22:55:29 +0900

    E05

--- src/main/java/com/example/mvc/controller/BoardContorller.java
+++ src/main/java/com/example/mvc/controller/BoardContorller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,24 +41,24 @@ public class BoardContorller {
 	private BoardService boardService;
 
 	/**
-	 * 목록
+	 * 게시물 목록 조회
 	 * 
 	 * @return
 	 */
 	@GetMapping
-	@Operation(summary = "목록 조회", description = "게시물 목록 정보를 조회할 수 있습니다.")
+	@Operation(summary = "게시물 목록 조회", description = "게시물 목록 정보를 조회할 수 있습니다.")
 	public BaseResponse<List<Board>> getList() {
 		return new BaseResponse<List<Board>>(boardService.getList());
 	}
 
 	/**
-	 * 상세정보
+	 * 게시물 상세 조회
 	 * 
 	 * @param boardSeq
 	 * @return
 	 */
 	@GetMapping("/{boardSeq}")
-	@Operation(summary = "상세 조회", description = "게시물 번호에 해당하는 상세 정보를 조회할 수 있습니다.")
+	@Operation(summary = "게시물 상세 조회", description = "게시물 번호에 해당하는 상세 정보를 조회할 수 있습니다.")
 	@Parameter(name = "boardSeq", description = "게시물 번호", example = "1")
 	public BaseResponse<Board> get(@PathVariable int boardSeq) {
 		Board board = boardService.get(boardSeq);
@@ -65,28 +66,22 @@ public class BoardContorller {
 	}
 
 	/**
-	 * 등록 / 수정
+	 * 게시물 등록/수정
 	 * 
 	 * @param board
 	 */
 	@PutMapping("/save")
-	@Operation(summary = "등록 / 수정 조회", description = "신규 게시물 저장 및 기존 게시물 업데이트가 가능합니다.")
-	@Parameters({ 
-		@Parameter(name = "boardSeq", description = "게시물 번호", example = "1"),
-		@Parameter(name = "title", description = "제목", example = "spring"),
-		@Parameter(name = "contents", description = "내용", example = "spring 강좌") })
-	public BaseResponse<Integer> save(BoardParameter param) {
-		log.info("contents raw = [{}]", param.getContents());
-		log.info("contents length = {}", param.getContents() != null ? param.getContents().length() : "null");
+	@Operation(summary = "게시물 등록/수정", description = "신규 게시물 저장 및 기존 게시물 업데이트가 가능합니다.")
+	public BaseResponse<Integer> save(@RequestBody BoardParameter param) {
 		// 제목 필수 체크
 		if (!StringUtils.hasText(param.getTitle())) {
-			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED, new String[] {"title","제목"});
+			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED, new String[] { "title", "제목" });
 		}
 		// 내용 필수 체크
 		if (!StringUtils.hasText(param.getContents())) {
-			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED, new String[] {"contents","내용"});
+			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED, new String[] { "contents", "내용" });
 		}
-		
+
 //		// null 처리
 //		if (board == null) {
 //			throw new BaseException(BaseResponseCode.DATA_IS_NULL, new String[] {"게시물"});
@@ -97,12 +92,12 @@ public class BoardContorller {
 	}
 
 	/**
-	 * 삭제
+	 * 게시물 삭제
 	 * 
 	 * @param boardSeq
 	 */
 	@DeleteMapping("/{boardSeq}")
-	@Operation(summary = "삭제", description = "게시물 번호에 해당하는 정보를 삭제합니다.")
+	@Operation(summary = "게시물 삭제", description = "게시물 번호에 해당하는 정보를 삭제합니다.")
 	@Parameter(name = "boardSeq", description = "게시물 번호", example = "1")
 	public BaseResponse<Boolean> delete(@PathVariable int boardSeq) {
 		Board board = boardService.get(boardSeq);
--- src/main/java/com/example/mvc/parameter/BoardParameter.java
+++ src/main/java/com/example/mvc/parameter/BoardParameter.java
@@ -1,11 +1,17 @@
 package com.example.mvc.parameter;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class BoardParameter {
-	private int boardSeq;
-	private String title;
-	private String contents;
+	@Schema(description = "게시물 번호", example = "1")
+    private int boardSeq;
+
+    @Schema(description = "제목", example = "제목입니다")
+    private String title;
+
+    @Schema(description = "내용", example = "내용입니다")
+    private String contents;
 }
